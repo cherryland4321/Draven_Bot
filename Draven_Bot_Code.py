f8b90140e6b875dcb80f31b3f85decaacee39db0
cherryland4321 <107452289+cherryland4321@users.noreply.github.com>
2023-01-14 18:12:59 -0600

    Add files via upload

--- Draven_Bot_Code.py
+++ Draven_Bot_Code.py
@@ -11,4 +11,4 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def test(ctx, arg):
     await ctx.send(arg)
 
-bot.run('MTA2MzY3MTY1NzU5Nzc3NTkzMg.GX_OoF.208yZBxQjALn8fvA8g365IKTfi6YnlrhYoyr9I')
+bot.run('TOKEN HERE')
